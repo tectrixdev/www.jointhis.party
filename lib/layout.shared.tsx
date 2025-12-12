b39894056262ceab8e5131b3a6510cbee3104c78
tectrixdev <joranhennion@duck.com>
2025-12-12 16:11:50 +0100

    Some more documentation updates.

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,9 +1,24 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { icons } from "lucide-react";
+import { Wrench } from "lucide-react";
+import { Speech } from "lucide-react";
 
 export function baseOptions(): BaseLayoutProps {
 	return {
 		nav: {
 			title: "JoinThis.Party",
 		},
+		links: [
+			{
+				text: "Tool",
+				url: "/tool",
+				icon: <Wrench />,
+			},
+			{
+				text: "Support / Discord",
+				url: "/discord",
+				icon: <Speech />,
+			},
+		],
 	};
 }
