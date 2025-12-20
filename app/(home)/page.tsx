b69a7a1a4a6ad82adb5512ee49e1acc7e5cd5300
tectrixdev <joranhennion@duck.com>
2025-12-20 23:57:14 +0100

    Update home page, add logo and some metadata.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,44 @@
 import Link from "next/link";
+import Form from "next/form";
+import Image from "next/image";
+import { Metadata } from "next";
 
 export default function HomePage() {
 	return (
-		<div className="flex flex-col justify-center text-center flex-1">
-			<h1 className="text-2xl font-bold mb-4">Hello World</h1>
-			<p>
-				You can open{" "}
-				<Link href="/docs" className="font-medium underline">
-					/docs
-				</Link>{" "}
-				and see the documentation.
-			</p>
-		</div>
+		<main className="bg-[url(/bg.webp)] flex flex-col justify-center text-center flex-1 bg-no-repeat bg-cover bg-center bg-fixed h-max w-screen"></main>
 	);
 }
+
+export const metadata: Metadata = {
+	title: "JoinThisParty",
+	description: "Start your self-hosting journey here! Free subdomain included!",
+	generator: "Next.js",
+	applicationName: "jointhis.party",
+	keywords: [
+		"Free domain",
+		"Free subdomain",
+		"Self hosting",
+		"TecTrix",
+		"Self hosting guides",
+		"self hosting minecraft server",
+		"minecraft self hosting",
+		"minecraft subdomain",
+		"Free srv record",
+		"How to self host",
+		"How to port forward",
+		"How to use a reverse proxy",
+	],
+	robots: "index, follow",
+	alternates: {
+		canonical: "https://www.jointhis.party",
+	},
+	authors: [{ name: "Joran Hennion" }],
+	creator: "Joran Hennion",
+	publisher: "Joran Hennion",
+	formatDetection: {
+		email: false,
+		address: false,
+		telephone: false,
+	},
+	metadataBase: new URL("https://www.jointhis.party"),
+};
--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -4,11 +4,25 @@ import { Wrench } from "lucide-react";
 import { Speech } from "lucide-react";
 import { BookOpenText } from "lucide-react";
 import { Home } from "lucide-react";
+import Image from "next/image";
 
 export function baseOptions(): BaseLayoutProps {
 	return {
 		nav: {
-			title: "JoinThis.Party",
+			title: (
+				<>
+					<Image
+						src="/favicon.ico"
+						alt="JoinThisParty"
+						width={32}
+						height={32}
+						priority
+						className="rounded-lg"
+					/>
+					<p>JoinThisParty</p>
+				</>
+			),
+			enabled: true,
 		},
 		links: [
 			{
