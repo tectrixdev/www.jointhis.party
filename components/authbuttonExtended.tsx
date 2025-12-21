3fa18b286cdec8e45654a92dd900cb20b74d9e67
tectrixdev <joranhennion@duck.com>
2025-12-22 00:40:03 +0100

    Small change.

--- components/authbuttonExtended.tsx
+++ components/authbuttonExtended.tsx
@@ -5,9 +5,10 @@ export default async function AuthbuttonExtended() {
 	const session = await auth();
 	return session ? null : (
 		<div
-			className={`cursor-pointer md:hidden bg-black/25 backdrop-blur-lg flex flex-row gap-3 border rounded-lg w-full p-2 m-2 text-xl self-center border-white`}
+			className={`cursor-pointer re md:hidden bg-black/25 backdrop-blur-lg flex flex-row gap-3 border rounded-lg w-full p-2 m-2 text-xl self-center border-white`}
 		>
 			<form
+				className="w-full"
 				action={async () => {
 					"use server";
 					await signIn();
@@ -15,7 +16,7 @@ export default async function AuthbuttonExtended() {
 			>
 				<button
 					type="submit"
-					className="text-white hover:text-fd-accent-foreground px-2 py-2"
+					className="text-white hover:text-fd-accent-foreground px-2 py-2 text-center w-full"
 				>
 					Sign in
 				</button>
