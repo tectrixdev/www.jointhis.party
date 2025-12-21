f91451cae72401685f46f922f4a82f9439185b71
tectrixdev <joranhennion@duck.com>
2025-12-21 15:32:34 +0100

    Finalization for beta deployment.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -10,6 +11,7 @@ export default function Layout({ children }: LayoutProps<"/">) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
 			<body className="flex flex-col min-h-screen">
+				<Analytics />
 				<RootProvider>{children}</RootProvider>
 			</body>
 		</html>
