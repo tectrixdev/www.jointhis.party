19ca9c0686c66e4cff7794f80914f0b15e1ba8cb
tectrixdev <joranhennion@duck.com>
2026-03-29 18:11:27 +0200

    Try other baseurl approach.

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,5 +1,3 @@
-export const baseUrl =
-  process.env.NODE_ENV === "development" ||
-  !process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? new URL("http://localhost:3000")
-    : new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
+export const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`)
+  : new URL("http://localhost:3000");
