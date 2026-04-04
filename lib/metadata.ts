09e9d7c1056e4929430e784d95b76b121c16a4c3
tectrixdev <joranhennion@duck.com>
2026-04-04 22:15:56 +0200

    Fix baseURL on beta.jointhis.party.

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -2,4 +2,6 @@ export const baseUrl =
   process.env.NODE_ENV === "development" ||
   !process.env.VERCEL_PROJECT_PRODUCTION_URL
     ? new URL("http://localhost:3000")
-    : new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
+    : process.env.VERCEL_TARGET_ENV == "preview"
+      ? new URL("https://beta.jointhis.party")
+      : new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
