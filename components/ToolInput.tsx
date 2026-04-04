44b5d52f2e7c7a2ce90500908b6acdbe09980bfe
tectrixdev <joranhennion@duck.com>
2026-04-04 15:47:38 +0200

    Add some more error handling.

--- components/ToolInput.tsx
+++ components/ToolInput.tsx
@@ -183,7 +183,7 @@ export function Manager() {
       }
     } catch (err: any) {
       console.error(err);
-      toast.error("Unexpected error, try again in one minute.");
+      toast.error(err.message || "Unexpected error, try again in one minute.");
     } finally {
       setLoadingRecords(false);
     }
