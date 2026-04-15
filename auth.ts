d563bb65920addaf07f0a624acef5bb0c62cd8de
tectrixdev <joranhennion@duck.com>
2026-04-16 00:48:19 +0200

    CLI route still depends on the UserIdFromAvatar function as it gets the session there, without custom fields I suppose, they might be there and the code might be unnecessary, I'll look into it.

--- auth.ts
+++ auth.ts
@@ -68,4 +68,22 @@ export type customSession = {
   id: string | undefined;
 };
 
+// There should be 2 types of avatar URLs:
+// https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png
+// https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}
 export const ValidateDiscordID = /^\d{17,30}$/; // snowflake = unix apparently, that's why newer accounts didn't work.
+export function UserIdFromAvatar(avatar: string | null | undefined) {
+  if (avatar === null || avatar === undefined) {
+    return undefined;
+  }
+  var userId;
+  const url = new URL(avatar);
+  if (ValidateDiscordID.test(url.pathname.split("/")[2])) {
+    userId = url.pathname.split("/")[2];
+  } else if (ValidateDiscordID.test((userId = url.pathname.split("/")[3]))) {
+    userId = url.pathname.split("/")[3];
+  } else {
+    userId = undefined;
+  }
+  return userId;
+}
